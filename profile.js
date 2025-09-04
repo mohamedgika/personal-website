57cbfd4771f943bf9adad22b0828675da8892ebd
mohamedgika <dodm773@gmail.com>
2025-09-04 23:10:18 +0300

    add new javascript

---
+++ profile.js
@@ -0,0 +1,23 @@
+let name = {
+    first_name: "Mohamed", // Intial Value
+    middle_name : "Ashraf", // Intial Value
+    last_name : "Sobhy",
+};
+
+let age = {
+    day : 1,
+    month : 1,
+    year : 2000,
+};
+
+let bank_account = {
+    name : "Mohamed Ashraf Sobhy",
+    balance : 0,
+    acount_number : 123456789,
+    account_type : "Savings",
+    CVV: 123,
+    expiry_date : "01/01/2023",
+}
+
+
+console.log(bank_account);
